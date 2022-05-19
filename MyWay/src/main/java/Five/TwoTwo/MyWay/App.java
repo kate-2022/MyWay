2ca1de2c7593bf83a7f2bb5fece46b51689061a6
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-05-19 14:45:24 +0200

    Update App.java

--- MyWay/src/main/java/Five/TwoTwo/MyWay/App.java
+++ MyWay/src/main/java/Five/TwoTwo/MyWay/App.java
@@ -1,20 +1,27 @@
+
 package Five.TwoTwo.MyWay;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App  {
 		
     public static void main( String[] args ) {
      
-        ApplicationContext context = new ClassPathXmlApplicationContext("Jobs.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         
-        FascinatingWorkable perfectJob = (FascinatingWorkable)context.getBean("javaDevelopment");
-        perfectJob.doWhatYouLove();
+      //  FascinatingWorkable perfectJob = (FascinatingWorkable)context.getBean("javaDevelopment");
+        
+      //  perfectJob.doWhatYouLove();
     	
+        JavaDevelopment iLike = context.getBean(JavaDevelopment.class);
+        
+        iLike.doWhatYouLove();
+        
        ((AbstractApplicationContext) context).close();
-    	
     	
     }
 }
